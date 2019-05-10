44420537e3b57de2373cae44d785fdaff321c38c
galk32 <49902643+galk32@users.noreply.github.com>
2019-05-10 17:34:11 +0300

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,12 @@
+#reading the files
+
+nei <- readRDS("summarySCC_PM25.rds")
+nei$year <- as.factor(nei$year)
+
+#making the plot
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+with(nei,boxplot(Emissions~year, outline = FALSE, col=c("blue","green","yellow","red"),
+                 ylab= "Emission", xlab="year", main="Total Emissions per year"))
+
+dev.off()
